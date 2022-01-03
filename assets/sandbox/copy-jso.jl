27330ac7bec8c8e76a5825b94ca982565e2548b0
abelsiqueira <nepper271@gmail.com>
2022-01-03 19:57:29 +0000

    Deploying to gh-pages from  @ 7a4af024f13eb395e4c7d0cd3afb745144ab12bf 🚀

--- assets/sandbox/copy-jso.jl
+++ assets/sandbox/copy-jso.jl
@@ -1,3 +1,4 @@
 # This file was generated, do not modify it. # hide
-cp("_assets/jso.png", "_assets/something.png"; force=true)
+isdir("plots") || mkdir("plots")
+cp("_assets/jso.png", "plots/something.png"; force=true)
 print("No errors")
