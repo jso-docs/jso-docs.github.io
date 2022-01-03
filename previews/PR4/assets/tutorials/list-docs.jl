bf70682c43a06756177df6b197ec5bbf6d207034
abelsiqueira <abelsiqueira@users.noreply.github.com>
2022-01-03 21:59:14 +0000

    Deploying to gh-preview from  @ e2f3caa2a2eebaa3c956aedc47de71b23feffde9 🚀

---
+++ previews/PR4/assets/tutorials/list-docs.jl
@@ -0,0 +1,10 @@
+# This file was generated, do not modify it. # hide
+#hideall
+using JSON
+data = JSON.parsefile("_data/docs.json")
+data = sort(data, by=x->x["date"], rev=true)
+for entry in data
+  title, repo = entry["title"], entry["repo"]
+  link = "https://jso-docs.github.io/$repo"
+  println("- [$title]($link)")
+end
