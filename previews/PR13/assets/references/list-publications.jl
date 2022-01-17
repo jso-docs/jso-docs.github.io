f3f293fe8390fce59e943cc9f0d74ef6ef0e4bac
abelsiqueira <abelsiqueira@users.noreply.github.com>
2022-01-17 22:13:43 +0000

    Deploying to gh-preview from  @ 798c0373562d6f047103a004d86d67c919ed3cf3 🚀

--- previews/PR13/assets/references/list-publications.jl
+++ previews/PR13/assets/references/list-publications.jl
@@ -1,4 +1,5 @@
 # This file was generated, do not modify it. # hide
+#hideall
 using Bibliography, JSON
 
 function bib_string(
@@ -6,38 +7,65 @@ function bib_string(
   title,
   where,
   date,
-  link=nothing,
+  link=nothing;
+  isdoi=false,
 )
-  if link != nothing
+  if !isdoi && link != nothing
     title = "[$title]($link)"
   end
-  str = join([title, "**$(authors)**", where, date], ", ")
-  str = replace(str, "{" => "", "}" => "", "\\'e" => "é", "\\student" => "")
+  V = [title, "**$(authors)**", where]
+  if date != ""
+    push!(V, date)
+  end
+  if isdoi
+    push!(V, "[$link]($link)")
+  end
+  str = join(V, ", ")
+  str = replace(str, "{" => "", "}" => "", "\\'e" => "é", "\\student" => "", "https://doi.org/" => "")
   return str
 end
 
-bib = bibtex_to_web("_data/references.bib")
+function icon(cite)
+  """~~~
+  <button data-clipboard-text="$(cite)" class="copy-ref">
+  <span class="icon is-small has-text-primary">
+  <ion-icon size="small" name="copy"></ion-icon>
+  </span>
+  </button>
+  ~~~"""
+end
+
+bib = vcat(bibtex_to_web.("_data/bibtex/" .* readdir("_data/bibtex/"))...)
 json = JSON.parsefile("_data/references.json")
 json = sort(json, by=x->x["date"], rev=true)
 
 for (type_, title) in [
     ("book", "Books"),
     ("article", "Articles"),
-    ("techreport", "Technical Reports"),
+    ("techreport", "Technical Reports")
+  ]
+  selected = sort(filter(x -> x.type == type_, bib), by=x -> x.year)
+  length(selected) == 0 && continue
+
+  println("### $title")
+
+  for v in selected
+    str = bib_string(v.names, v.title, v.in, "", v.link, isdoi=true)
+
+
+    println("- " * icon(v.cite) * str)
+  end
+end
+
+for (type_, title) in [
     ("talk", "Talks"),
     ("classes", "Classes"),
   ]
-  println("### $title")
-  #First bibtex
-  selected = sort(filter(x -> x.type == type_, bib), by=x -> x.year)
-  for v in selected
-    str = bib_string(v.names, v.title, v.in, "", v.link)
-    icon = """~~~\n<button data-clipboard-text="$(v.cite)" class="button is-small is-rounded is-primary copy-ref"><span class="icon is-small has-text-white"><ion-icon size="large" name="copy"></ion-icon></span></button>\n~~~"""
-    println("- " * icon * str)
-  end
-
-  #Second json
   selected = filter(x -> x["type"] == type_, json)
+  length(selected) == 0 && continue
+
+  println("### $title")
+
   for v in selected
     str = bib_string(v["author"], v["title"], v["where"], Dates.format(Date(v["date"]), "yyyy-u-d"), get(v, "link", nothing))
     println("- " * str)
