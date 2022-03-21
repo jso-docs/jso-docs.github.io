e5809675bae868dbc088ad5b39c9ebcafc35b98e
abelsiqueira <nepper271@gmail.com>
2022-03-21 21:50:45 +0000

    Deploying to gh-pages from  @ d21a1defb8f3bf3fce112a240db7cef8ed1d93a9 🚀

--- assets/tutorials/list-docs.jl
+++ assets/tutorials/list-docs.jl
@@ -4,7 +4,19 @@ using JSON
 data = JSON.parsefile("_data/docs.json")
 data = sort(data, by=x->x["date"], rev=true)
 for entry in data
-  title, repo = entry["title"], entry["repo"]
+  title, repo, short = entry["title"], entry["repo"], entry["short"]
   link = "https://jso-docs.github.io/$repo"
-  println("- [$title]($link)")
+  println("""~~~
+  <div class="news-item">
+    <a href="$link">
+      <span class="is-size-4 has-text-primary">
+        $title
+      </span>
+      <br>
+      <p class="has-text-grey-dark is-size-6">
+        $short
+      </p>
+    </a>
+  </div>
+  ~~~""")
 end
